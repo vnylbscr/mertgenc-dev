2dadd0bd4507d75178f6d6a1a9ebafa3b13c3490
Mertqpien <mert@qpien.com>
2021-10-27 00:19:29 +0300

    fix build errors

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,14 +2,16 @@ import { createClient, createImageUrlBuilder } from 'next-sanity';
 
 const config = {
    dataset: 'production',
-   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
    useCdn: process.env.NODE_ENV === 'production',
 };
 
+//@ts-ignore
 export const sanityClient = createClient(config);
 
+//@ts-ignore
 export const urlFor = (source: any) => createImageUrlBuilder(config).image(source);
-
+//@ts-ignore
 export const previewClient = createClient(config);
 
 export const getClient = (usePreview?: any) => (usePreview ? previewClient : sanityClient);
